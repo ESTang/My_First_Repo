3b78fab20d485437b60541019647e5c86d76e0cf
NightWalkerM3 <tanges@hotmail.com>
2024-11-23 09:48:46 -0500

    Update Test_Add_files.py
    Modifying tanges@gmail.com

--- Test_Add_files.py
+++ Test_Add_files.py
@@ -1,3 +1,7 @@
 # This is a basic python script that print the current working directory
 import os
 print ("Hi, the current work directory is: " + os.getcwd())
+
+# Adding a hello world test
+print("Hi there from tanges@hotmail,com")
+
